dc2f45b050f3c1c6c51cb3a551e7883ace40dc43
SheikhaJ <120921603+SheikhaJ@users.noreply.github.com>
2023-05-09 22:57:23 +0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,94 +1,129 @@
-﻿namespace Hello_world
+﻿using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Class2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //print the first line in c#
-            Console.WriteLine("Hello, World!");
+            int[] arr = new int[3];
+            arr[0] = 1;
+            arr[1] = 2;
+            arr[2] = 3;
+            Console.WriteLine(arr[0]);
+            Console.WriteLine(arr[1]);
+            Console.WriteLine(arr[2]);
 
-            //this is a single comment
-            /* this is
-            * a multipule
-            line comments
-            */
+            int[] arr2 = new int[3] { 6, 4, 5 };
+            Console.WriteLine(arr2[0]);
 
-            //variable decralation
-            string myname = "sheikha"; 
-            Console.WriteLine(myname); 
+            string[] strings = { "code", "academy", "C#" };
+            Console.WriteLine(strings[0]);
 
-            //there are different type of variables in c#
+            //multi dimention
+            int[,] matrix = new int[2, 2];
+            matrix[0, 0] = 12;
+            matrix[0, 1] = 24;
+            matrix[1, 0] = 30;
+            matrix[1, 1] = 41;
+            Console.WriteLine(matrix[0,0]);
 
-            string name = "sheikha"; //string used to create charecter, text. myname is a variable where we store the text
-            int Num = 5;  // stores integers, without decimals like (147) (78)
-            double DoubleNum = 5.99D; //stores floating point numbers, with decimals like (147.3) (78.00)
-            char Letter = 'D';  //stores single characters,
-            bool myBool = true; //stores values with two states: true or false
+            //jugged array
+            int[][] JuggedA = new int[3][];
+            JuggedA[0] = new int[] { 21, 32, 32 };
+            JuggedA[1] = new int[] { 37, 49 };
+            JuggedA[2] = new int[] { 26, 37, 58, 19 };
+            Console.WriteLine(JuggedA[0]);
 
-            //to create 2 integer and 2 object and use GetHashCode
-            //Object.GetHashCode() used to insert and identify an object in a hash-based collection
+            //Jagged array using initializer list
+            /* int[][] JuggedA1 = new int[][] {
+                 new int[] { 0, 20, 30 };
+                 new int[] { 35, 45 };
+                 new int[] { 61, 71, 81 }; 
+              }; */
 
-            int a = 2;
-            int b = 3;
-            Console.WriteLine(a.GetHashCode());
-            Console.WriteLine(b.GetHashCode());
+            
+            //array sliceing
+            int[] numbers = new int[] { 11,22,33,44,55 }; 
 
-            //This method is used to find the hash code for this instance.
+            int[] slice = numbers[2..4];
+            Console.WriteLine(slice[0]);
 
-            object obj = new object(); // declaring an object
-            object obj1 = new object();
-            Console.WriteLine(obj.GetHashCode());
-            Console.WriteLine(obj1.GetHashCode());
+            int[] slice1 = numbers[2..^2];
+            Console.WriteLine(slice1[0]);
 
-            //String Concatenation: add variable together
-            string name1 = "Sheikha";
-            int age = 25;
-            string job = " full stack";
-            Console.WriteLine($"Hello " + name + "your age is " + age + " and you are " + job);
-            //make it easier to print
-            Console.WriteLine($"Hello {name} your age is {age} and you are{job}");
+            int[] slice2 = numbers[..2];
+            Console.WriteLine(slice2[0]);
+
+            int[] slice3 = numbers[2..];
+            Console.WriteLine(slice3[0]);
 
 
-            //different between use dynamic & var
+            //even or add
+            int x = 10;
+            string y =(x % 2 == 0) ? "even" : "odd";
+            Console.WriteLine(y);
 
-            dynamic s2 = "ss";
-            s2 = 2;
-            Console.WriteLine(s2.GetHashCode());
+            //Null Value
+            int? z = null;
+            Console.WriteLine(z);
 
-            /* 
-             var s2 = "ss";
-            s2 = 2;
-            Console.WriteLine(s2.GetHashCode());  //it will give error because in var it will take s2 as string
-             */
 
-            /* Booleans
-             * boolean type is declared with the bool keyword
-             */
+            string str = null;
+            Console.WriteLine(str);
 
-            int x = 15; //we use the assignment operator (=) to assign the value 15 to a variable called x
-            int y = 20;
+            //
+            string str1 = null;
+            str1 = (str1 == null) ? "coding" : str1;
+            Console.WriteLine(str1);
 
-            bool equal = x == y;
-            bool notEqul = x != y;
-            bool lessThan = x < y;
-            bool greaterThan = x > y;
-            bool lessThanOrEqual = x <= y;
-            bool greaterThanOrEqual = x >= y;
+            string str11 = null;
+            str11 = str11 ?? "coding";
+            Console.WriteLine(str11);
 
-            Console.WriteLine(equal); //false
-            Console.WriteLine(notEqul); //true
-            Console.WriteLine(lessThan);  //true
-            Console.WriteLine(greaterThan);  //false
-            Console.WriteLine(lessThanOrEqual); //true
-            Console.WriteLine(greaterThanOrEqual); //false
+            //
 
-            /*logical xor operaters
-             * ^ is logical exclusive or
-            */
-            Console.WriteLine(true ^ true);    // output: False
-            Console.WriteLine(true ^ false);   // output: True
-            Console.WriteLine(false ^ true);   // output: True
-            Console.WriteLine(false ^ false);  // output: False
+            /*
+            string str12 = null;
+            Console.WriteLine(str12.ToUpper());
+            */ //error
+
+            //
+            string str12 = null;
+            Console.WriteLine(str12?.ToUpper());
+
+            //statemrnts & block statement
+            int k = 5;
+            {
+                int r = 6;
+                Console.WriteLine(r);
+            } 
+            Console.WriteLine(y);
+
+            //increment and Decrement
+            int x1 = 1;
+            int y1 = 2;
+            //Prefix
+            Console.WriteLine(++x1);
+            Console.WriteLine(--y1);
+
+            //Postfix
+            Console.WriteLine(x1++);
+            Console.WriteLine(y1--);
+
+            //Conditioning
+            //if statement: program to check the final projecr>=85% and absence >= 75% from first try
+            
+
+
+
+
+
+
+
+
+
 
 
 
