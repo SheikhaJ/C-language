24f7faf933386a55f8f0e46a9f724002a636c767
SheikhaJ <120921603+SheikhaJ@users.noreply.github.com>
2023-05-15 21:36:44 +0400

    calculate the net salary

---
+++ lecture5/lecture5/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lecture5
+{
+    public class Employee
+    {
+        public const double TAX = 0.03; //constant: hold fixid value
+        public string firstName; //public is access modifier , can access and modified from class memeber
+        public string lastName;
+        public double loggedHours;
+        public double wig;
+        public int addOne(int x) //methode: add function
+        {
+            return x+1;
+        }
+
+    }
+    
+}
---
+++ lecture5/lecture5/Program.cs
@@ -0,0 +1,66 @@
+﻿
+namespace lecture5
+{
+
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee(); //make a copy of employee class
+            
+            
+            Console.WriteLine("Enter no of employees:"); //to get the size of array used to save each epmloyee information
+            int empNumber = Convert.ToInt32(Console.ReadLine());
+
+            Employee[] arr = new Employee[empNumber]; 
+
+            for (int i = 0; i < empNumber; i++)
+            {
+                //get input information from user
+                Console.WriteLine("First name:"); 
+                employee.firstName = Console.ReadLine();
+
+                Console.WriteLine("Last name:");
+                employee.lastName = Console.ReadLine();
+
+                Console.WriteLine("loggedHours:");
+                employee.loggedHours = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Wig:");
+                employee.wig = Convert.ToDouble(Console.ReadLine());
+
+                double totalSalary = employee.loggedHours * employee.wig; //claculate the total salary
+                double taxSalary = totalSalary * Employee.TAX; //calculate the tax salary using constant value from employee class and calle it by the main class
+                double netSalary = totalSalary - taxSalary; //calculate the net salary
+
+                Console.WriteLine($"your salary ={netSalary}");
+            } 
+
+
+
+
+
+            /*Console.WriteLine("First employee salary:");
+            
+            employee.firstName = "joe";
+            employee.lastName = "ter";
+            employee.loggedHours = 10;
+            employee.wig= 10;
+
+            double totalSalary= employee.loggedHours * employee.wig;
+            double taxSalary = totalSalary *   Employee.TAX;
+            double netSalary = totalSalary - taxSalary;
+
+            Console.WriteLine($"your salary ={netSalary}");*/
+
+
+
+
+
+
+
+
+    }
+}
+}
