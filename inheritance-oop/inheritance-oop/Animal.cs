a8af2b59153f52d8c8b2e703300c0bfa4be57f76
SheikhaJ <120921603+SheikhaJ@users.noreply.github.com>
2023-05-16 15:07:37 +0400

    Add files via upload

---
+++ inheritance-oop/inheritance-oop/Animal.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance_oop //inheritance & polymorphysem
+{
+    abstract class Animal  //create class with common attribute name and age and methode named move //make the class abstract 
+    {
+        public string Name { get; set; }
+        public int age { get; set; }
+            
+          public virtual void move() //make it override which mean this methode does some behavior and different behavior in childe class
+        {
+            Console.WriteLine("moving "); 
+        }
+
+        public abstract void MakeSound(); //it will be error if we don't calle it in all childe classes
+    }
+
+        class Cat : Animal //create class from the base class it will inherte the attribute from animal class with addition attribute
+        {
+            public string gender;
+            public void sound()
+            {
+                Console.WriteLine("meo!!");
+            } 
+            public override void move()
+        {
+            base.move();
+            Console.WriteLine("hey i am moving now..");
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("this is my voice !");
+        }
+
+        public override string ToString() //ToString() method. Types commonly override the Object. to return a string that represents the object instance
+        {
+            return $"my name is {this.Name}";
+        }
+
+
+
+    }
+
+
+
+
+    class Dog : Animal //create another sub class
+        {
+            public string category;
+            public void sound()
+            {
+                Console.WriteLine("woof!!");
+
+            }
+           public override void move()
+            {
+            base.move();
+        Console.WriteLine("hey i am moving :) ");
+             }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("howhowhow!!!!");
+        }
+    }
+
+        sealed class Eagle: Animal //sealed class to make it not inheitable
+        {
+            public string category;
+        public void flying() //this class has different methode but it can move also because it  
+        {
+            Console.WriteLine("flying");
+        }
+
+
+           public override void move()
+           {
+            base.move();
+             Console.WriteLine("hey i am moving");
+            }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("this is my voice !");
+        }
+    }
+    
+}
---
+++ inheritance-oop/inheritance-oop/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace inheritance_oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create object from cat class
+            Cat cat = new Cat();
+            cat.Name = "lolo";
+            cat.age = 5;
+            cat.gender = "femal";
+            Console.WriteLine($"{cat.Name} {cat.age} {cat.gender} ");
+            cat.move();
+            cat.sound();
+            cat.MakeSound();
+            Console.WriteLine(cat.ToString()); 
+
+            //create object from Dog class
+            Dog dog = new Dog();
+            dog.Name = "ddd";
+            dog.age = 10;
+            dog.category = "rrr";
+            Console.WriteLine($"{dog.Name} {dog.age} {dog.category} ");
+            dog.move();
+            dog.sound();
+            dog.MakeSound();
+
+
+
+
+
+
+        }
+    }
+}
